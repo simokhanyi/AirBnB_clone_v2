5369226f6d768cbe725b0d5862b6b7ac3ccfa517
Simo Khanyi <simokhanyi@gmail.com>
2024-01-17 08:58:33 -0800

    AirBnb

--- models/city.py
+++ models/city.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-        places = relationship(
-            'Place',
-            cascade="all, delete, delete-orphan",
-            backref='cities'
-        )
     else:
         state_id = ""
         name = ""
