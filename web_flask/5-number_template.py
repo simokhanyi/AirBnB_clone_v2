b3d53fa48314f260b52e0d96c10b44a16cee1a25
Simo Khanyi <simokhanyi@gmail.com>
2024-02-20 00:26:34 +0000

    Web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,15 +15,18 @@ def hello():
     """Route to display 'Hello HBNB!'."""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     """Route to display 'HBNB'."""
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def c_text(text):
     """Route to display 'C ' followed by the value of the text variable."""
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>')
@@ -31,15 +34,18 @@ def python_text(text):
     """Route to display 'Python ' followed by value of the text variable."""
     return "Python " + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>')
 def number(n):
     """Route to display '<n> is a number' if n is an integer."""
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>')
 def number_template(n):
     """Route to display a HTML page with the number."""
     return render_template('5-number.html', n=n)
+
 
 # Running the Flask app
 if __name__ == '__main__':
