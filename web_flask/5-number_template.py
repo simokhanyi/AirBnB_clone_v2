8b65090cbc1f5f0a87a4e1a18554edf9586f0331
Simo Khanyi <simokhanyi@gmail.com>
2024-02-20 00:11:48 +0000

    Web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>')
 def number_template(n):
     """Route to display a HTML page with the number."""
-    return render_template('5-number_template.html', n=n)
+    return render_template('5-number.html', n=n)
 
 # Running the Flask app
 if __name__ == '__main__':
