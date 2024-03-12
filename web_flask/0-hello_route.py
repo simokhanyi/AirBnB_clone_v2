2a87629899d1e6b574acd81a564a9f9ebc68643a
Simo Khanyi <simokhanyi@gmail.com>
2024-03-12 03:23:20 +0000

    Application server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!' when accessing root URL."""
     return 'Hello HBNB!'
